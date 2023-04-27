280da27ae98928cd40073ca8abade1d169dab555
y00tss <shepel9119@gmail.com>
2023-04-27 23:37:37 +0300

    task 2

---
+++ lesson 2/task 1.py
@@ -0,0 +1,10 @@
+import datetime
+
+# date info
+today = datetime.date.today()
+
+
+# user info
+name = input("Please, write you name: ")
+
+print(f"Hello {name}! {today} is a perfect day to learn some python.")
