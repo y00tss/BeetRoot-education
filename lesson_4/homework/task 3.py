0d359ec318b604b434384631b4b4511c3ecc7a79
y00tss <shepel9119@gmail.com>
2023-05-08 17:35:32 +0300

    Update Task 3.py

--- lesson_4/homework/task 3.py
+++ lesson_4/homework/task 3.py
@@ -2,9 +2,9 @@ import random
 
 user_input = input()
 
-for i in range(len(user_input)):                                                                                        # count of actions depend from string
+for i in range(5):                                                                                        # count of actions depend from string
    user_list = list(user_input)                                                                                         # to list for shuffling
    random.shuffle(user_list)
    shuffled_string = "".join(user_list)                                                                                 # and back
 
-   print(shuffled_string, end=" ")                                                                                      # printing in line by "end=" ""
+   print(shuffled_string, end=" ")                                                                              # printing in line by "end=" ""
