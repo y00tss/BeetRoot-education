70d470e47991787c693462df01f3fb883d84fefc
y00tss <shepel9119@gmail.com>
2023-05-23 00:28:59 +0300

    I DID IT but )))

--- lesson_10/homework/task_2/new.py
+++ lesson_10/homework/task_2/new.py
@@ -1,8 +1,5 @@
-"""Я не успел. Оказалось, что модифицировать готовый код сложнее, чем создавать новый. Я доделаю в течении нескольких дней( до след урока)
-Ровно сутки пытался сделать так, чтобы работала функция добавления. Но никак не работал. Причина была в том, что я вручную создавал новый готовый json. Когда взял из урока и изменил данные - все заработало(((("""
 import json
 import sys
-from file import *
 
 
 def load_from_json():
@@ -26,7 +23,7 @@ def main():
                  '2 - Searching of data: \n'
                  '3 - Update of data: \n'
                  '4 - Deleting of data: \n'
-                 '5 - exit\n -- ')
+                 '5 - exit\n ==> ')
     match mode:
         case '1':
             add_data(phone_book)
@@ -35,10 +32,10 @@ def main():
             select_data(phone_book, operation)
         case '3':
             operation = 'update'
-            modify_data(phone_book)
+            modify_data(phone_book, operation)
         case '4':
             operation = 'delete'
-            delete_data()
+            delete_data(phone_book, operation)
         case '5':
             operation = 'exit'
             exit()
@@ -51,20 +48,23 @@ def main():
 
 
 def add_data(phone_book):
-    phone_number = input("Enter Phone number: ")
+    phone_number = input("Enter Phone number: +")
     first_name = input("Enter first name: ")
     last_name = input("Enter last name: ")
     full_name = first_name + " " + last_name
     location = input("Enter your region code: ")
     if phone_number.isdigit() and first_name.isalpha() and last_name.isalpha():
-        print("Data is right")
-        insert_data = {"Phone number": int(phone_number),
-                       "First name": first_name,
-                       "Last name": last_name,
-                       "Full name": first_name + " " + last_name,
-                       "Region": location
-                       }
-        phone_book['Phone Book'].append(insert_data)
+        if check_data_integrity(phone_number, phone_book):
+            print("Data is right")
+            insert_data = {"Phone number": int(phone_number),
+                           "First name": first_name,
+                           "Last name": last_name,
+                           "Full name": first_name + " " + last_name,
+                           "Region": location
+                           }
+            phone_book['Phone Book'].append(insert_data)
+        else:
+            print("This number already registered")
     else:
         print("Was entered incorrect data.")
 
@@ -72,16 +72,20 @@ def add_data(phone_book):
 def select_data(phone_book, operation):
     selector_mode = input(f"Select that are you searching: \n"
                           '1 - Region\n'
-                          '2 - Full name\n'
-                          '3 - Phone number\n')
+                          '2 - First name\n'
+                          '3 - Last name\n'
+                          '4 - Phone number\n')
     match selector_mode:
         case '1':
             field = 'Region'
             print_selected_data(phone_book, field, operation)
         case '2':
-            field = 'Full name'
+            field = 'First name'
             print_selected_data(phone_book, field, operation)
         case '3':
+            field = 'Last name'
+            print_selected_data(phone_book, field, operation)
+        case '4':
             field = 'Phone number'
             print_selected_data(phone_book, field, operation)
         case default:
@@ -92,7 +96,9 @@ def print_selected_data(phone_book, field, operation):
     value = input(f" Input {field} to {operation} user: ")
     if field == 'Region':
         print_text(phone_book, value, field, operation)
-    elif field == 'Full name':
+    elif field == 'First name':
+        print_text(phone_book, value, field, operation)
+    elif field == 'Last name':
         print_text(phone_book, value, field, operation)
     elif field == 'Phone number':
         print_number(phone_book, value, field, operation)
@@ -105,6 +111,8 @@ def print_text(phone_book, value, field, operation):
                 print(i)
             else:
                 phone_book['Phone Book'].remove(i)
+        else:
+            print(f"{value} is not registered")
 
 
 def print_number(phone_book, value, field, operation):
@@ -115,28 +123,35 @@ def print_number(phone_book, value, field, operation):
                 return i
             else:
                 phone_book['Phone Book'].remove(i)
+        else:
+            print("Dont have this number")
 
 
-
-def modify_data(phone_book):
-    phone_number = input("Enter phone number, that you want to update: ")
+def modify_data(phone_book, phone_number):
+    phone_number = input("Enter phone number, that you want to update: +")
     if phone_number.isdigit():
         if check_data_integrity(phone_number, phone_book):
-            print(f"Dont have this {phone_number}")
+            print(f"Dont have this {phone_number} phone number...")
         else:
             field = 'Phone number'
-            fields = input("With comma enter parameters, that you want to update: ").title()
-            try:
-                for field in fields.split(","):
-                    value = input(f"Enter new parameter for {field}: ")
-                    value = int(value) if field in ['Phone number'] else value
-                    if field == 'Phone number' and not check_data_integrity(value, phone_book):
-                        print(f"Phone number - {phone_number} already registered")
-###################################################################################################################################################
-            except:
-                print("Wrong parameters")
+            record = print_int_data(phone_book, phone_number, field)
+            type_type = input('Enter parameters, that you want to update: \n"1" - Region \n"2" - Full name \n ==>')
+            match type_type:
+                case "1":
+                    value = input(f"Enter new region for phone {phone_number}: ")
+                    record['Region'] = value
+                case "2":
+                    try:
+                        first_name = input("Enter first name: ")
+                        last_name = input("Enter last name: ")
+                        for entry in phone_book["Phone Book"]:
+                            entry["First name"] = first_name
+                            entry["Last name"] = last_name
+                            entry["Full name"] = f"{first_name} {last_name}"
+                    except:
+                        print("Wrong data")
     else:
-        print("Wrong data")
+        print("Phone number should be a digits")
 
 
 def check_data_integrity(phone_number, phone_book):
@@ -144,6 +159,23 @@ def check_data_integrity(phone_number, phone_book):
         if i['Phone number'] == int(phone_number):
             return False
     return True
+
+
+def print_int_data(phone_book, value, field):
+    for i in phone_book['Phone Book']:
+        if int(value) == i[f'{field}']:
+            print(i)
+            return i
+
+
+def delete_data(phone_book, operation):
+    phone_number = int(input("Enter phone, that data you want to delete: +"))
+    if not check_data_integrity(phone_number, phone_book):
+        phone_book["Phone Book"] = [entry for entry in phone_book["Phone Book"] if entry["Phone number"] != phone_number]
+        print(f'All data that related to number {phone_number} was deleted!')
+    else:
+        print(f"This number - {phone_number} is not registered")
+
 
 
 def exit_program():
